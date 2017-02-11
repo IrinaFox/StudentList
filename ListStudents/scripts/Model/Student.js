e430455ebb4755ec38f2659b8e0c3ba5c09c8543
Liss <irina.fox88@gmail.com>
2017-02-12 01:47:28 +0200

    Working birthdate

--- ListStudents/scripts/Model/Student.js
+++ ListStudents/scripts/Model/Student.js
@@ -1,22 +1,22 @@
 //Constructor for creating students
-function Student (name, lastName, gender, skype, phone, email, age, birthdayDate) {
+function Student (name, lastName, gender, skype, phone, email, birthday) {
     this.name = name;
     this.lastName = lastName;
     this.gender = gender;
     this.skype = skype;
     this.phone = phone;
     this.email = email;
-    this.age = age;
-    this.birthdayDate = new Date(1997, 1, 1);
+    this.birthdayDate = new Date(birthday);
 
     this.toJSON = function () {
-      var fullForm = {};
+      var date = new Date(),
+          fullForm = {};
 
       fullForm.name = name;
       fullForm.lastName = lastName;
       fullForm.fullName = name  + " " +lastName;
       fullForm.gender = gender;
-      fullForm.age = new Date() - birthdayDate;
+      fullForm.age = date.getFullYear() - this.birthdayDate.getFullYear();
       fullForm.skype = skype;
       fullForm.email = email;
       fullForm.phone = phone;
--- ListStudents/scripts/Model/StudentList.js
+++ ListStudents/scripts/Model/StudentList.js
@@ -1,13 +1,13 @@
 //function for creating array with list of students
 function StudentList () {
-    this.irina = new Student("Irina", 'Ruban', "female", "princess_rina88", "+380505610891", "irina.fox88@gmail.com", '22');
-    this.svetlana = new Student("Svetlana", 'Ovcharenko', "female", "ami-caty", "+380663334455", "svetlana.sunny@gmail.com", '18');
-    this.polina = new Student("Polina", 'Zhylyaeva', "female", "polina_volna", "+380934445566", "poluina.catty3@gmail.com", '18');
-    this.elena = new Student("Elena", 'Ryndina', "female", "izaya256", "+380666668888", "elena.flower123@gmail.com", '18');
-    this.alexey = new Student("Alexey", 'Grigoryev', "male", "dn100980rev", "+380998885511", "alexey.rembo25@gmail.com", '20');
-    this.vladimir = new Student("Vladimir", 'Timofeev', "male", "vovanium", "+380634207163", "vladivir.terminator55@gmail.com", '20');
+    var irina = new Student("Irina", 'Ruban', "female", "princess_rina88", "+380505610891", "irina.fox88@gmail.com", '1995,2,4'),
+        svetlana = new Student("Svetlana", 'Ovcharenko', "female", "ami-caty", "+380663334455", "svetlana.sunny@gmail.com", '1997,1,1'),
+        polina = new Student("Polina", 'Zhylyaeva', "female", "polina_volna", "+380934445566", "poluina.catty3@gmail.com", '1997,1,1'),
+        elena = new Student("Elena", 'Ryndina', "female", "izaya256", "+380666668888", "elena.flower123@gmail.com", '1997,1,1'),
+        alexey = new Student("Alexey", 'Grigoryev', "male", "dn100980rev", "+380998885511", "alexey.rembo25@gmail.com", '1997,1,1'),
+        vladimir = new Student("Vladimir", 'Timofeev', "male", "vovanium", "+380634207163", "vladivir.terminator55@gmail.com", '1997,1,1');
 
-    this.students = [this.irina, this.svetlana, this.polina, this.elena, this.alexey, this.vladimir];
+    this.students = [irina, svetlana, polina, elena, alexey, vladimir];
 
     return this;
 }
