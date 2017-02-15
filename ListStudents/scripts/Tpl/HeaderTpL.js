4225a79f371d493a214d703fc9ec1e2fd744250f
Liss <irina.fox88@gmail.com>
2017-02-15 04:41:01 +0200

    Add edit to tml

--- ListStudents/scripts/Tpl/HeaderTpL.js
+++ ListStudents/scripts/Tpl/HeaderTpL.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var headerTpl = '<div class="miniDiv">FULL NAME<img src="pics/name.png" class="pic"> </img></div>\
-                 <div class="miniDiv">SKYPE<img src="pics/skype.png" class="pic"> </img></div>\
-                 <div class="miniDiv">GENDER<img src="pics/gender.png" class="pic"> </img></div>\
-                 <div class="miniDiv">MORE</div>';
+var headerTpl = `<div class="miniDiv">FULL NAME<img src="pics/name.png" class="pic"> </img></div>
+                 <div class="miniDiv">SKYPE<img src="pics/skype.png" class="pic"> </img></div>
+                 <div class="miniDiv">GENDER<img src="pics/gender.png" class="pic"> </img></div>
+                 <div class="miniDivButton">MORE</div>
+                 <div class="miniDivButton">EDIT</div>`;
--- ListStudents/scripts/Tpl/ItemTpL.js
+++ ListStudents/scripts/Tpl/ItemTpL.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var itemTpl = '<div class="miniDiv">:fullName</div>\
+var itemTpl = `<div class="miniDiv">:fullName</div>\
                <div class="miniDiv">:skype</div>\
                <div class="miniDiv">:gender</div>\
-               <div class="miniDiv"><input type="button" value="More" class="button"/></div>';
+               <div class="miniDivButton"><input type="button" value="More" class="button"/></div>
+               <div class="miniDivButton"><input type="button" value="Edit" class="button"/></div>`;
