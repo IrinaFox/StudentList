f3d2785052c72d388e996c00985a16ad2533b002
Liss <irina.fox88@gmail.com>
2017-03-05 21:39:01 +0200

    Add continents

--- ListStudents/script/application/CountryList/Tpl/HeaderCountyListTpl.js
+++ ListStudents/script/application/CountryList/Tpl/HeaderCountyListTpl.js
@@ -2,7 +2,7 @@
 
 var headerCountryListTpl = `<div class="miniDiv">COUNTRY</img></div>
                  <div class="miniDiv">POPULATION</div>
-                 <div class="miniDiv">AREA</div>
+                 <div class="miniDiv">AREA sq.mi.</div>
                  <div class="miniDivButton">LIKE</div>
                  <div class="miniDivButton" title="delete from this window">DISLAKE</div>
                  <div class="miniDivButton" title="absolutely delete country">DELETE</div>`;
--- ListStudents/script/application/CountryList/Tpl/ParserCountries.js
+++ ListStudents/script/application/CountryList/Tpl/ParserCountries.js
@@ -1,19 +1,61 @@
 'use strict';
 
 function parserCountries () {
-    var countriesListCSV = countriesTpl.split(/\r?\n/),
+    var countriesListCSV = countriesSCV.split(/\r?\n/),
+        regions = parserContinents(),
         countryList = [];
+
+
 
     countriesListCSV.forEach(function (country) {
         var countryData = country.split('	'),
             countryName = countryData[0],
-            continent = countryData[1],
+            region = countryData[1],
             population = countryData[2],
             area = countryData[3],
-            newCountry = new Country(countryName, population, area, continent);
+            continent,
+            newCountry;
 
+        if (region === regions[0] || region === regions[7] || region === regions[8]) {
+            continent = 'Asia';
+        } else if (region === regions[1] || region === regions[4] || region === regions[10]) {
+            continent = 'Europa';
+        } else if (region === regions[2] || region === regions[5]) {
+            continent = 'Africa';
+        } else if (region === regions[3]) {
+            continent = 'Australia';
+        } else if (region === regions[6] || region === regions[9]) {
+            continent = 'America';
+        }
+
+        newCountry = new Country(countryName, population, area, continent);
         countryList.push(newCountry);
     });
 
     return countryList;
+}
+
+function parserContinents () {
+    var continents = countriesSCV.split(/\r?\n/),
+        uniqueContinents =[],
+        i;
+
+    continents.forEach(function (continent) {
+        var region = continent.split('	')[1];
+
+        if (uniqueContinents.length === 0) {
+            uniqueContinents.push(region);
+        } else {
+            for (i = 0; i < uniqueContinents.length; i++) {
+                if (region === uniqueContinents[i]) {
+                    break;
+                }
+                if (i === (uniqueContinents.length - 1)) {
+                    uniqueContinents.push(region);
+                }
+            }
+        }
+    });
+
+    return uniqueContinents;
 }
